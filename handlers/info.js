546df88e3b4ff640cd1d4b17842078140d4d15ef
kulakowka <kulakowka@gmail.com>
2016-03-20 06:49:46 +0300

    save

--- handlers/info.js
+++ handlers/info.js
@@ -2,9 +2,7 @@
 
 module.exports = {
   show (socket) {
-    console.log('show')
     socket.on('page:findOne', (data, respond) => {
-      console.log('page:findOne', data)
       var page = data.id
       var html = '<h1>' + page + '</h1><p>content</p>'
       socket.emit('page:' + page + ':update', { html })
